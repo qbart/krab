d56277c7fd28cc952402eed8cc42799c32667adc
qbart <wojtowicz.bartlomiej@gmail.com>
2020-01-07 04:55:11 +0100

    Add root module

--- krab.go
+++ krab.go
@@ -1,19 +1,3 @@
 package krab
 
-import (
-	"fmt"
-
-	HclParser "github.com/hashicorp/hcl/hcl/parser"
-)
-
 const Version = "0.0.1"
-
-func main() {
-	fmt.Println("Krab v{}", Version)
-
-	ast, err := HclParser.Parse([]byte(""))
-	if err != nil {
-		panic("Failed to parse")
-	}
-	fmt.Println("test {}", ast)
-}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/qbart/krab"
+)
+
+func main() {
+	fmt.Println("Krab v{}", krab.Version)
+}
